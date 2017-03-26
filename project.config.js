cd7810a6cd10438a4a96e18b871192141df03f4e
mateuszarchicinski <mateusz.archicinski@gmail.com>
2017-03-26 15:00:12 +0200

    Added more descriptive comments to a few variables

--- project.config.js
+++ project.config.js
@@ -1,24 +1,24 @@
 // PROJECT CONFIG
 module.exports = {
-    APP_NAME: "app", // 
-    LANGUAGES: [ // Required to create templates in different languages. Remember to create directories with the same name (src/template/views/pl - en) and to configure Data File
+    APP_NAME: "app", // Value of directive (ng-app) to auto-bootstrap an AngularJS application.
+    LANGUAGES: [ // Required to create templates in different languages. Remember to create directories with the same name (src/template/views/pl - en) and to configure Data File.
         'pl', // First element is default value of language
         'en'
     ],
-    BASE_URL: '/', // Adds tag <base href="BASE_URL"> inside <head> only when a variable is not empty
-    HOST: '', // Example host which is required for search engine optimization (SEO) ---> https://www.example-host.pl/
+    BASE_URL: '/', // Adds tag <base href="BASE_URL"> inside <head> only when a variable is not empty.
+    HOST: '', // Production host of your domain which is required for tags <meta property="og:url content="HOST"> and <meta property="og:image" content="HOST + tile-1200x630px.png">.
     CONFIG_FILE: 'project.config.js',
     DATA_FILE: 'project.data.json',
     DIRECTORY: {
-        WORK_DIR: 'src', // 
-        DIST_DIR: 'dist', // 
-        TEST_DIR: 'test' // 
+        WORK_DIR: 'src', // Workflow directory
+        DIST_DIR: 'dist', // Distribution directory
+        TEST_DIR: 'test' // Test directory
     },
-    BROWSER_SYNC: {
-        TARGET: 'http://at-workflow', // 
-        PORT: 3000, // 
-        LOG_PREFIX: 'AT', // 
-        LOG_CONNECTIONS: true // 
+    BROWSER_SYNC: { // Custom configuration of module BROWSER SYNC
+        TARGET: 'http://at-workflow',
+        PORT: 3000,
+        LOG_PREFIX: 'AT',
+        LOG_CONNECTIONS: true
     },
     FTP_CONFIG: { // All variables required to upload files into FTP Server
         HOST: '',
@@ -27,12 +27,12 @@ module.exports = {
         DESTINATION: '/public_html/'
     },
     API_KEYS: {
-        TINIFY: '' // 
+        TINIFY: '' // To use the API you must provide your API key. You can get an API key here ---> https://tinypng.com/developers
     },
     GOOGLE_ANALYTICS: {
-        TRACKING_ID: '' // Adds tracking script only when a variable is not empty
+        TRACKING_ID: '' // Adds tracking script only when a variable is not empty.
     },
     FACEBOOK_APPS: {
-        APP_ID: '' // Adds tag <meta property="fb:app_id" content="FACEBOOK_APPS.APP_ID"> inside <head> only when a variable is not empty
+        APP_ID: '' // Adds tag <meta property="fb:app_id" content="FACEBOOK_APPS.APP_ID"> inside <head> only when a variable is not empty.
     }
 };
